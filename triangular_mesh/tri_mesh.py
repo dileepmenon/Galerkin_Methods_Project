176f5e6f0da23cfebc3bbe76a327a31d8fd5e7bd
Dileep Menon <dileepmenon92@yahoo.com>
2017-03-25 16:51:23 +0530

    Modify function to return triangle edge length
    This is needed to calculate the Courant number

--- triangular_mesh/tri_mesh.py
+++ triangular_mesh/tri_mesh.py
@@ -9,6 +9,6 @@ def mesh_rect(tri_length = 0.3, x_dim = 5.0, y_dim = 2.5):
     length = tri_length
     p, v = mt.RectangleSegments([0, 0], [x_dim, y_dim], edge_length = length)
     mesh_points, mesh_elements = mt.DoTriMesh(p, v, edge_length = length)
-    return mesh_points, mesh_elements
+    return mesh_points, mesh_elements, length
 
 
