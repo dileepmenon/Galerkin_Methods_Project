5801ae01989c3666f0dde572502047ed6f06c700
Dileep Menon <dileepmenon92@yahoo.com>
2017-03-26 14:35:18 +0530

    Add functions to compute K_uh and K_vh matrices

--- triangular_mesh/Matrices.py
+++ triangular_mesh/Matrices.py
@@ -13,23 +13,31 @@ def global_mass_matrix(mesh_points, mesh_elements):
     """Computes and returns the global mass matrix"""
     me_pt = mesh_points
     me_el = mesh_elements
+
     #Number of nodes in mesh
     num_nodes = len(mesh_points)
     global_matrix = np.zeros((num_nodes, num_nodes))
+
     for i in me_el:
         A = tri_area([me_pt[i[0]][0], me_pt[i[1]][0], me_pt[i[2]][0]],
                      [me_pt[i[0]][1], me_pt[i[1]][1], me_pt[i[2]][1]],
                     )
+
         #Mass matrix of element
         M_e = (A/12.0)*(np.mat('2 1 1; 1 2 1; 1 1 2'))
+
         for n, index in enumerate(i):
             row = global_matrix[index]
+
             #Other elements in list i excluding index
             oth_el = filter(lambda x: x!=index, i)
+
             a = M_e[n].item(n)
+
             row[index] += a
             row[oth_el[0]] += 0.5*a
             row[oth_el[1]] += 0.5*a
+
     return np.mat(global_matrix)
 
 
@@ -42,9 +50,11 @@ def global_K_hu_matrix(mesh_points, mesh_elements, H):
     """
     me_pt = mesh_points
     me_el = mesh_elements
+
     #Number of nodes in mesh
     num_nodes = len(mesh_points)
     global_matrix = np.zeros((num_nodes, num_nodes))
+
     for i in me_el:
         y_23, y_31, y_12 = (me_pt[i[1]][1] - me_pt[i[2]][1],
                             me_pt[i[2]][1] - me_pt[i[0]][1],
@@ -55,16 +65,21 @@ def global_K_hu_matrix(mesh_points, mesh_elements, H):
                        [y_12, y_12, y_12]
                       ]
                      )
+
         #K_hu matrix of element
         K_hu_e = (H/6.0)*(m)
+
         for n, index in enumerate(i):
             #Other elements in list i excluding index
             oth_el = filter(lambda x: x!=index, i)
+
             row = global_matrix[index]
             a = K_hu_e[n].item(n)
+
             row[index] += a
             row[oth_el[0]] += a
             row[oth_el[1]] += a
+
     return -1*np.matrix(global_matrix)
 
 
@@ -77,9 +92,11 @@ def global_K_hv_matrix(mesh_points, mesh_elements, H):
     """
     me_pt = mesh_points
     me_el = mesh_elements
+
     #Number of nodes in mesh
     num_nodes = len(mesh_points)
     global_matrix = np.zeros((num_nodes, num_nodes))
+
     for i in me_el:
         x_32, x_13, x_21 = (me_pt[i[2]][0] - me_pt[i[1]][0],
                             me_pt[i[0]][0] - me_pt[i[2]][0],
@@ -90,14 +107,104 @@ def global_K_hv_matrix(mesh_points, mesh_elements, H):
                        [x_21, x_21, x_21]
                       ]
                      )
+
         #K_hu matrix of element
         K_hu_e = (H/6.0)*(m)
+
+        for n, index in enumerate(i):
+            #Other elements in list i excluding index
+            oth_el = filter(lambda x: x!=index, i)
+
+            row = global_matrix[index]
+            a = K_hu_e[n].item(n)
+
+            row[index] += a
+            row[oth_el[0]] += a
+            row[oth_el[1]] += a
+
+    return -1*np.matrix(global_matrix)
+
+
+
+def global_K_uh_matrix(mesh_points, mesh_elements):
+    """Computes the coefficient matrix of h in the weak form of the
+    x-momentum equation
+    """
+    #Acceleration due to gravity
+    g = 9.8
+
+    me_pt = mesh_points
+    me_el = mesh_elements
+
+    #Number of nodes in mesh
+    num_nodes = len(mesh_points)
+    global_matrix = np.zeros((num_nodes, num_nodes))
+
+    for i in me_el:
+        y_23, y_31, y_12 = (me_pt[i[1]][1] - me_pt[i[2]][1],
+                            me_pt[i[2]][1] - me_pt[i[0]][1],
+                            me_pt[i[0]][1] - me_pt[i[1]][1]
+                           )
+        m = np.matrix([[y_23, y_31, y_12],
+                       [y_23, y_31, y_12],
+                       [y_23, y_31, y_12]
+                      ]
+                     )
+
+        #K_uh matrix of element
+        K_uh_e = (g/6.0)*(m)
+
         for n, index in enumerate(i):
             #Other elements in list i excluding index
             oth_el = filter(lambda x: x!=index, i)
             row = global_matrix[index]
-            a = K_hu_e[n].item(n)
+            a = K_uh_e[n].item(n)
             row[index] += a
-            row[oth_el[0]] += a
-            row[oth_el[1]] += a
+            row[oth_el[0]] += K_uh_e[n].item(list(i).index(oth_el[0]))
+            row[oth_el[1]] += K_uh_e[n].item(list(i).index(oth_el[1]))
+
+    return np.matrix(global_matrix)
+
+
+def global_K_vh_matrix(mesh_points, mesh_elements):
+    """Computes the coefficient matrix of v in the weak form of the
+    y-momentum equation
+    """
+    #Acceleration due to gravity
+    g = 9.8
+
+    me_pt = mesh_points
+    me_el = mesh_elements
+
+    #Number of nodes in mesh
+    num_nodes = len(mesh_points)
+    global_matrix = np.zeros((num_nodes, num_nodes))
+
+    for i in me_el:
+        x_32, x_13, x_21 = (me_pt[i[2]][0] - me_pt[i[1]][0],
+                            me_pt[i[0]][0] - me_pt[i[2]][0],
+                            me_pt[i[1]][0] - me_pt[i[0]][0]
+                           )
+        m = np.matrix([[x_32, x_13, x_21],
+                       [x_32, x_13, x_21],
+                       [x_32, x_13, x_21]
+                      ]
+                     )
+
+        #K_vh matrix of element
+        K_vh_e = (g/6.0)*(m)
+
+        for n, index in enumerate(i):
+            #Other elements in list i excluding index
+            oth_el = filter(lambda x: x!=index, i)
+
+            row = global_matrix[index]
+            a = K_vh_e[n].item(n)
+
+            row[index] += a
+            row[oth_el[0]] += K_vh_e[n].item(list(i).index(oth_el[0]))
+            row[oth_el[1]] += K_vh_e[n].item(list(i).index(oth_el[1]))
+
     return -1*np.matrix(global_matrix)
+
+
